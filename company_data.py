e685cda97c51b713cf5a6c906336ad4b03f63451
takoz <tuonomule@gmail.com>
2022-02-13 02:25:00 +0300

    Adding indicators and historical data

--- company_data.py
+++ company_data.py
@@ -6,7 +6,10 @@ CompanyDataRow = namedtuple('CompanyDataRow', ['title', 'value', 'compact'])
 
 class CompanyData:
 
+    _valid = False
+
     _data = []
+    _historical_data = []
 
     _company_id = None
     _currency = None
@@ -51,8 +54,82 @@ class CompanyData:
             CompanyDataRow('Текущая стоимость акции', r.json()['asset']['last_price'], True)
         )
 
+        r = requests.get(
+            'https://findale.pro/api/report?company_id={}&currency={}&section=ind&type=Y'.format(
+                self._company_id, self._currency
+            )
+        )
+        if r.status_code != 200:
+            print('Could not get {} info: {}'.format(ticker, r.status_code))
+            return
+
+        last_market_cap = r.json()['last_q']['last_q_data']['market_cap']
+        last_profit = r.json()['last_q']['last_q_data']['profit_net']
+        last_ocf = r.json()['last_q']['last_q_data']['cash_oper_activities_net']
+        last_total_equity = r.json()['last_q']['last_q_data']['total_equity']
+        last_total_assets = r.json()['last_q']['last_q_data']['total_assets']
+        last_net_debt = r.json()['last_q']['last_q_data']['debt_net']
+        last_ebitda = r.json()['last_q']['last_q_data']['ebitda']
+        last_revenue = r.json()['last_q']['last_q_data']['revenue']
+        roe = last_profit / last_total_equity * 100
+        roa = last_profit / last_total_assets * 100
+        p_e = last_market_cap / last_profit
+        p_ocf = last_market_cap / last_ocf
+        p_bv = last_market_cap / last_total_equity
+        p_s = last_market_cap / last_revenue
+        net_debt_ebitda = last_net_debt / last_ebitda
+
+        self._historical_data = []
+        for period in r.json()['data']:
+            year = period['year']
+            p = period['data']['market_cap']
+            e = period['data']['profit_net']
+            ocf = period['data']['cash_oper_activities_net']
+            total_equity = period['data']['total_equity']
+            total_assets = period['data']['total_assets']
+            net_debt = period['data']['debt_net']
+            ebitda = period['data']['ebitda']
+            revenue = period['data']['revenue']
+
+            self._historical_data.append(
+                (
+                    ' /{}/'.format(year),
+                    '{:.1f}'.format(p / e),
+                    '{:.1f}'.format(p / ocf),
+                    '{:.1f}'.format(p / total_equity),
+                    '{:.1f}'.format(p / revenue),
+                    '{:.1f}'.format(e / total_equity * 100),
+                    '{:.1f}'.format(e / total_assets * 100),
+                    '{:.3f}'.format(net_debt / ebitda),
+                )
+            )
+
+        self._data.append(
+            CompanyDataRow('(P/E)', '{:.1f}'.format(p_e), True)
+        )
+        self._data.append(
+            CompanyDataRow('(P/OCF)', '{:.1f}'.format(p_ocf), True)
+        )
+        self._data.append(
+            CompanyDataRow('(P/BV)', '{:.1f}'.format(p_bv), True)
+        )
+        self._data.append(
+            CompanyDataRow('(P/S)', '{:.1f}'.format(p_s), True)
+        )
+        self._data.append(
+            CompanyDataRow('(ROE)', '{:.1f}'.format(roe), True)
+        )
+        self._data.append(
+            CompanyDataRow('(ROA)', '{:.1f}'.format(roa), True)
+        )
+        self._data.append(
+            CompanyDataRow('(NET DEBT/EBITDA)', '{:.1f}'.format(net_debt_ebitda), True)
+        )
+
+        self._valid = True
+
     def is_valid(self):
-        return self._company_id is not None
+        return self._valid
 
     def get_titles(self):
         if self._compact:
@@ -65,4 +142,13 @@ class CompanyData:
             return [str(d.value) for d in self._data if d.compact]
         else:
             return [str(d.value) for d in self._data]
+
+    def get_historical_values(self):
+        return self._historical_data
+
+    def get_historical_offset(self):
+        if self._compact:
+            return 4
+        else:
+            return 6
 
--- parser.py
+++ parser.py
@@ -3,7 +3,9 @@ import requests
 
 from company_data import CompanyData
 
-def get_offset_string(data):
+dltr = '  '
+
+def get_offset_strings(data):
     titles = data[0].get_titles()
 
     lens = []
@@ -12,29 +14,51 @@ def get_offset_string(data):
         for d in data:
             lens[i] = max(len(d.get_values()[i]), lens[i])
 
-    return '  '.join('{{:{}}}'.format(l) for l in lens)
+    hist_lens = [0, ]
+    for i in range(data[0].get_historical_offset()):
+        hist_lens[0] += (lens[i] + len(dltr))
+    hist_lens[0] -= len(dltr)
+    for i in range(data[0].get_historical_offset(), len(lens)):
+        hist_lens.append(lens[i])
 
-def report(data):
+    return (
+        lens, dltr.join('{{:{}}}'.format(l) for l in lens),
+        hist_lens, '{{:>{}}}{}{}'.format(
+            hist_lens[0], dltr, dltr.join('{{:{}}}'.format(l) for l in hist_lens[1:])
+        )
+    )
+
+def report(data, historical):
     if not len(data):
         print('Dataset is empty')
 
-    offsets_string = get_offset_string(data)
+    lens, offsets_string, hist_lens, hist_offsets_string = get_offset_strings(data)
+    total_len = sum(lens) + len(lens)*len(dltr) - len(dltr)
+
     print(offsets_string.format(*data[0].get_titles()))
+    print('-' * total_len)
     for d in data:
         print(offsets_string.format(*d.get_values()))
+        if not historical:
+            continue
+        for v in d.get_historical_values():
+            print(hist_offsets_string.format(*v))
+        print('-' * total_len)
 
-def main(compact, tickers):
+
+def main(compact, historical, tickers):
     data = []
     for t in tickers:
         d = CompanyData(t, compact)
         if d.is_valid():
             data.append(d)
-    report(data)
+    report(data, historical)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--compact', action='store_true', help='compact view')
+    parser.add_argument('-i', '--historical', action='store_true', help='show historical data')
     parser.add_argument('tickers', metavar='T', type=str, nargs='+', help='tickers')
     args = parser.parse_args()
 
-    main(args.compact, args.tickers)
+    main(args.compact, args.historical, args.tickers)
